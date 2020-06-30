afcfbff4f2d4c3bce9a6fabb22402659a6be945f
Fernanda Ek <fernandaek@icloud.com>
2020-06-30 14:20:24 +0200

    Updates

---
+++ precache-manifest.e64da997b9cbc9f5804e9a96c7128db4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21473a70358a7e2c6dee232c7936b15",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9b004be6521d33b1487d",
+    "url": "/portfolio/static/css/2.5f72f936.chunk.css"
+  },
+  {
+    "revision": "3ecc3e6f6995ed456f4e",
+    "url": "/portfolio/static/css/main.2089240b.chunk.css"
+  },
+  {
+    "revision": "9b004be6521d33b1487d",
+    "url": "/portfolio/static/js/2.92125587.chunk.js"
+  },
+  {
+    "revision": "5403b55c1cb422a72be68a013c0b9049",
+    "url": "/portfolio/static/js/2.92125587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ecc3e6f6995ed456f4e",
+    "url": "/portfolio/static/js/main.3842b532.chunk.js"
+  },
+  {
+    "revision": "084dd7477ca23920be7e",
+    "url": "/portfolio/static/js/runtime-main.e99de2b6.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/portfolio/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/portfolio/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
